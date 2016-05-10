fbc0c643f4e0833b73bd442af498696266f1465a
bnasser oneo <bnasser.o@oneoweb.com>
2016-05-10 23:16:00 +0100

    main layout

--- footer.php
+++ footer.php
@@ -1,1 +1,16 @@
-<h3>footer</h3>
+<section id="footer">
+	<div class="ribbon"></div>
+	<div class="footer-content">
+		<div class="ui stackable grid container">
+			<div class="ui four wide column">aaabb</div>
+			<div class="ui four wide column">aaabb</div>
+			<div class="ui four wide column">aaabb</div>
+			<div class="ui four wide column">aaabb</div>
+		</div>
+	</div>
+</section>
+<section id="copyright">
+	<div class="ui container">
+		
+	</div>
+</section>
